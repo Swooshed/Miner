74ac2c617374b27ad2347cb310bbdca7f6dbe858
David McGillicuddy <dmcgill9071@gmail.com>
2015-06-11 14:15:47 +0100

    VBO demo is working

--- Miner Project/Render.fs
+++ Miner Project/Render.fs
@@ -6,49 +6,49 @@ open Pencil.Gaming
 open Pencil.Gaming.Graphics 
 
 type Game () =
-    member this.Run () =
-        if not (Glfw.Init ()) then raise (new System.Exception "GLFW initialisation failed")
-        let mutable window = Glfw.CreateWindow(800, 600, "Miner test", GlfwMonitorPtr.Null, GlfwWindowPtr.Null)
-        if window = GlfwWindowPtr.Null then raise (new System.Exception "Window creation failed")
-
+    // Initalise GLFW and context
+    do if not (Glfw.Init ()) then raise (new System.Exception "GLFW initialisation failed")
+    let window = Glfw.CreateWindow(800, 600, "Miner test", GlfwMonitorPtr.Null, GlfwWindowPtr.Null)
+    do  if window = GlfwWindowPtr.Null then
+            raise (new System.Exception "Window creation failed")
         Glfw.MakeContextCurrent window
         Glfw.SwapInterval 1
+
+    // Set up VBOs
+    let mutable vertexArrayID = 0
+    let mutable vertexBufferID = 0
+    let vertexBufferData = [|
+            -1.f; -1.f; 0.f;
+             1.f; -1.f; 0.f;
+             0.f;  1.f; 0.f; |] 
+    do  GL.GenVertexArrays (1, &vertexArrayID)
+        GL.BindVertexArray vertexArrayID
+
+        GL.GenBuffers (1, &vertexBufferID)
+        GL.BindBuffer (BufferTarget.ArrayBuffer, vertexBufferID)
+        let bufferSize = nativeint (sizeof<float32> * vertexBufferData.Length)
+        GL.BufferData (BufferTarget.ArrayBuffer, bufferSize, vertexBufferData, BufferUsageHint.StaticDraw)
+
+    interface System.IDisposable with
+        member this.Dispose() =
+            Glfw.DestroyWindow window
+            Glfw.Terminate ()
+
+    member this.Run () =
+
 
         // set error callback
         // set input callback
 
         while not (Glfw.WindowShouldClose window) do
-            let mutable width = 0
-            let mutable height = 0
-            Glfw.GetFramebufferSize (window, &width, &height)
-            let ratio = float width / float height
+            GL.EnableVertexAttribArray 0
+            GL.BindBuffer (BufferTarget.ArrayBuffer, vertexBufferID)
+            GL.VertexAttribPointer (0, 3, VertexAttribPointerType.Float, false, 0, nativeint 0)
 
-            GL.Viewport (0, 0, width, height)
-            GL.Clear ClearBufferMask.ColorBufferBit
-
-            GL.MatrixMode MatrixMode.Projection
-            GL.LoadIdentity ()
-            GL.Ortho(-ratio, ratio, -1., 1., 1., -1.)
-
-            GL.MatrixMode MatrixMode.Modelview
-
-            GL.LoadIdentity ()
-            GL.Rotate(float32 (Glfw.GetTime()) * 50.f, 0.f, 0.f, 1.f)
-
-            GL.Begin (BeginMode.Triangles)
-            GL.Color3 (1.f, 0.f, 0.f)
-            GL.Vertex3 (-0.6f, -0.4f, 0.f)
-            GL.Color3 (0.f, 1.f, 0.f)
-            GL.Vertex3 (0.6f, -0.4f, 0.f)
-            GL.Color3 (0.f, 0.f, 1.f)
-            GL.Vertex3(0.f, 0.6f, 0.f)
-            GL.End()
+            GL.DrawArrays (BeginMode.Triangles, 0, 3)
+            GL.DisableVertexAttribArray 0
 
 
-
-
-            Glfw.SwapBuffers(window)
+            Glfw.SwapBuffers window
             Glfw.PollEvents()
 
-        Glfw.DestroyWindow window
-        Glfw.Terminate ()
