211ac2ba26c7231e34dc3af031fff6f2b5dfb4c0
David McGillicuddy <dmcgill9071@gmail.com>
2015-06-11 15:33:27 +0100

    Have basic shaders working

---
+++ Miner Project/LoadShaders.fs
@@ -0,0 +1,35 @@
+﻿module Miner.LoadShaders
+
+open Pencil.Gaming.Graphics
+
+let loadShaders (vertexPath : string) (fragmentPath : string) =
+    let compileAndCheckShader path shaderMode =
+        // Compile
+        printf "Compiling shader: %s\n" path
+        let mutable iden = GL.CreateShader shaderMode
+        let code = System.IO.File.ReadAllText path
+
+        GL.ShaderSource (iden, code) // this line is not like the tutorial
+        GL.CompileShader iden
+
+        // Check
+        let log = GL.GetShaderInfoLog (int iden)
+        printf "%s\n" log
+        iden
+    let vertexShaderID = compileAndCheckShader vertexPath ShaderType.VertexShader
+    let fragmentShaderID = compileAndCheckShader fragmentPath ShaderType.FragmentShader
+
+    printf "Linking program\n"
+    let mutable programID = GL.CreateProgram ()
+    GL.AttachShader (programID, vertexShaderID)
+    GL.AttachShader (programID, fragmentShaderID)
+    GL.LinkProgram programID
+
+    let log = GL.GetProgramInfoLog (int programID)
+    printf "%s\n" log
+
+    GL.DeleteShader vertexShaderID
+    GL.DeleteShader fragmentShaderID
+
+    programID
+
--- Miner Project/Render.fs
+++ Miner Project/Render.fs
@@ -1,22 +1,33 @@
 ﻿module Miner.Render
 
 open Miner.SparseVoxelOctree
+open Miner.LoadShaders
 
 open Pencil.Gaming
 open Pencil.Gaming.Graphics 
 
 type Game () =
     // Initalise GLFW and context
-    do if not (Glfw.Init ()) then raise (new System.Exception "GLFW initialisation failed")
+    do  if not (Glfw.Init ()) then raise (new System.Exception "GLFW initialisation failed")
+        Glfw.WindowHint(WindowHint.Samples, 4)
+        Glfw.WindowHint(WindowHint.ContextVersionMajor, 3)
+        Glfw.WindowHint(WindowHint.ContextVersionMinor, 3)
+        Glfw.WindowHint(WindowHint.OpenGLForwardCompat, 1)
+        // FIXME: Glfw.WindowHint(WindowHint.OpenGLProfile, 1) XXX
+
+
     let window = Glfw.CreateWindow(800, 600, "Miner test", GlfwMonitorPtr.Null, GlfwWindowPtr.Null)
     do  if window = GlfwWindowPtr.Null then
             raise (new System.Exception "Window creation failed")
         Glfw.MakeContextCurrent window
         Glfw.SwapInterval 1
 
+        GL.ClearColor Color4.DarkBlue
+
     // Set up VBOs
     let mutable vertexArrayID = 0
     let mutable vertexBufferID = 0
+    let programID = loadShaders "SimpleVertexShader.glsl" "SimpleFragmentShader.glsl"
     let vertexBufferData = [|
             -1.f; -1.f; 0.f;
              1.f; -1.f; 0.f;
@@ -41,10 +52,12 @@ type Game () =
         // set input callback
 
         while not (Glfw.WindowShouldClose window) do
+            GL.Clear (ClearBufferMask.ColorBufferBit ||| ClearBufferMask.DepthBufferBit)
             GL.EnableVertexAttribArray 0
             GL.BindBuffer (BufferTarget.ArrayBuffer, vertexBufferID)
             GL.VertexAttribPointer (0, 3, VertexAttribPointerType.Float, false, 0, nativeint 0)
 
+            GL.UseProgram(programID)
             GL.DrawArrays (BeginMode.Triangles, 0, 3)
             GL.DisableVertexAttribArray 0
 
